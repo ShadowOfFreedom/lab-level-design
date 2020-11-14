7a6260ec40767bd522c0b11e63487f53fba7e701
Wojciech Wojtkielewicz <wojtkielewicz.wojciech@gmail.com>
2020-11-15 00:50:09 +0100

    Level 4 complet

---
+++ Assets/praca-12529-Wojciech_Wojtkielewicz/Scripts/KeyLogic.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Moe.Tools;
+using UnityEngine;
+
+public class KeyLogic : MonoBehaviour
+{
+    public GameObject FindKeyUiAndLogic;
+    public GameObject KeyObject;
+    public GameObject FinishBlocker;
+
+    void OnJointBreak(float breakForce)
+    {
+        DestroyKey();
+        OpenExit();
+    }
+
+
+    private void DestroyKey()
+    {
+        FindKeyUiAndLogic.SetActive(true);
+        Destroy(FindKeyUiAndLogic, 2);
+        Destroy(KeyObject, 1);
+    }
+
+    private void OpenExit()
+    {
+        Destroy(FinishBlocker);
+    }
+}
